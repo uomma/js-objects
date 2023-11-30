eaed817f75462a38d58bb167b79f0555bf7988bc
uomma <marcelloleone975@gmail.com>
2023-11-30 12:32:25 +0100

    array personaggi

---
+++ README.md
@@ -0,0 +1,8 @@
+crea array di oggetti per personaggi serie
+
+ogni personaggio deve avere: nome, forza, foto.
+
+1) crea array di oggetti con le info
+2) stampa in console per ogni membro del team le sue info
+3) stampa le stesse info sul dom
+
--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
 'use strict'
 
-const person = [
-    'kobe',
-    'brayant',
-    39,
-    'm',
-    true,
-    false,
-];
+const dragonball = {
+    personaggi: [
+        {
+            nome: 'Goku',
+            forza: '10000',
+            foto: 'goku.img',
+        },
+        {
+            nome: 'Vegeta',
+            forza: '9500',
+            foto: 'vegeta.img ',
 
-console.log(person )
+        },
+        {
+            nome: 'Crilin',
+            forza: '2000',
+            foto: 'crilin.img',
+
+        },
+        {
+            nome: 'Gohan',
+            forza: '8000',
+            foto: 'Gohan.img',
+
+        },
+        {
+            nome: 'Majin Boo',
+            forza: '8500',
+            foto: 'majin boo.img',
+
+        },
+        {
+            nome: 'Trunks',
+            forza: '7400',
+            foto: 'trunks.img',
+
+        },
+    ]
+}
+
+console.log(dragonball)
