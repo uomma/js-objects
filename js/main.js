aacff7b362fa24a0718c1514f3cfc67f84be842f
uomma <marcelloleone975@gmail.com>
2023-11-30 13:35:53 +0100

    stampa in console ogni oggetto di array

--- js/main.js
+++ js/main.js
@@ -1,43 +1,51 @@
 'use strict'
 
-const dragonball = {
-    personaggi: [
-        {
-            nome: 'Goku',
-            forza: '10000',
-            foto: 'goku.img',
-        },
-        {
-            nome: 'Vegeta',
-            forza: '9500',
-            foto: 'vegeta.img ',
+const dragonball = [
 
-        },
-        {
-            nome: 'Crilin',
-            forza: '2000',
-            foto: 'crilin.img',
+    {
+        nome: 'Goku',
+        forza: '10000',
+        foto: 'goku.img',
+    },
+    {
+        nome: 'Vegeta',
+        forza: '9500',
+        foto: 'vegeta.img ',
 
-        },
-        {
-            nome: 'Gohan',
-            forza: '8000',
-            foto: 'Gohan.img',
+    },
+    {
+        nome: 'Crilin',
+        forza: '2000',
+        foto: 'crilin.img',
 
-        },
-        {
-            nome: 'Majin Boo',
-            forza: '8500',
-            foto: 'majin boo.img',
+    },
+    {
+        nome: 'Gohan',
+        forza: '8000',
+        foto: 'Gohan.img',
 
-        },
-        {
-            nome: 'Trunks',
-            forza: '7400',
-            foto: 'trunks.img',
+    },
+    {
+        nome: 'Majin Boo',
+        forza: '8500',
+        foto: 'majin boo.img',
 
-        },
-    ]
-}
+    },
+    {
+        nome: 'Trunks',
+        forza: '7400',
+        foto: 'trunks.img',
 
-console.log(dragonball)
+    },
+
+]
+
+/* console.log(personaggi[0].nome)
+ */
+
+for (let i = 0; i < dragonball.length; i++) {
+
+    const caracter = dragonball[i];
+    console.log(caracter)
+
+} 
